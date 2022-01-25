36ce10e734352984012d8f36518a3347401e805f
nospacethere <nospacethere@gmail.com>
2022-01-25 21:16:12 +0900

    현섭:렌딩페이지 작업

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,58 @@
 <script>
-    const patternVue = "/img/pattern_svelte.png";
-    import { Button } from 'sveltestrap/src';
+	const svgVue = [
+		'/svg/landing/ground.svg',
+		'/svg/landing/leftBoard.svg',
+		'/svg/landing/rightBoard.svg',
+		'/svg/landing/veradiTitle1.svg',
+		'/svg/landing/veradiTitle2.svg',
+		'/svg/landing/veradiTitle3.svg',
+    '/svg/landing/man.svg'
+	];
+	import { Container, Col, Row, Image } from 'sveltestrap/src';
 </script>
 
-<section class="header relative pt-16 items-center flex h-screen max-h-860-px">
+<!---
+<div style="background-image: url({sgvVue[0]}); height: 500px; background-position: center 60%; background-size: 140%; background-repeat: no-repeat;">
+-->
+
+<Image alt=".." src={svgVue[0]} draggable="false" style="position:absolute; width:100%;" />
+
+<Container>
+  <Row style="position:relative">
+    <p class="py-5" />
+    <Col sm=0 md=0 lg=1/>
+    <Col sm=4 md=4 lg=4 xl=5>
+      <p class="py-4" />
+      <Image alt=".." src={svgVue[3]} draggable="false" />
+      <p class="py-3" />
+      <Image alt=".." src={svgVue[4]} draggable="false" />
+      <p class="py-4" />
+      <Image alt=".." src={svgVue[5]} draggable="false" />
+    </Col>
+    <Col sm=2 md=2>
+      <Image alt=".." src={svgVue[6]} draggable="false" style="position:absolute; float:right; height:85%" />
+    </Col>
+  </Row>
+</Container>
+
+
+
+
+
+
+
+<!---
+    <img
+        class="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
+        src="{sgvVue[0]}"
+        alt="..."
+    />
+    <img
+        class="float-left"
+        src="{sgvVue[1]}"
+        alt="..."
+    />
+      <section class="header relative pt-16 items-center flex h-screen max-h-860-px">
     <div class="container mx-auto items-center flex flex-wrap">
       <div class="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
         <div class="pt-32 sm:pt-0">
@@ -28,38 +77,14 @@
             학생 자신의 궁극의 시간가치와 결과가치를 구현합니다.
           </p>
           <div class="mt-12">
-            <Button
-              href="https://www.creative-tim.com/learning-lab/tailwind/svelte/overview/notus?ref=ns-index"
-              class="go-to-veradi text-white font-bold px-6 py-3 mb-1 uppercase shadow"
-            >
-              베라디 지원하기
-            </Button>
-            <Button
-              color="danger"
-              href="https://github.com/creativetimofficial/notus-svelte?ref=ns-index"
-              class="ml-1 px-6 py-3 mb-1 bg-blueGray-700 uppercase shadow"
-            >
-              Github Star
-            </Button>
           </div>
         </div>
       </div>
     </div>
-  
-    <img
-        class="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
-        src="{patternVue}"
-        alt="..."
-    />
   </section>
-
-
-
-  
-
-  <style>
-    :global(.go-to-veradi) {
-    background-color:rgba(248, 113, 113, 0.7);
-    }
-
-  </style>
+    -->
+<style>
+	:global(.go-to-veradi) {
+		background-color: rgba(248, 113, 113, 0.7);
+	}
+</style>
