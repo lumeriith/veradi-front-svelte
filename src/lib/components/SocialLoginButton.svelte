4701033e30e22fb03edc7c3b8731137d95467a37
nospacethere <nospacethere@gmail.com>
2022-02-09 17:06:22 +0900

    현섭:로그인페이지완성

---
+++ src/lib/components/SocialLoginButton.svelte
@@ -0,0 +1,30 @@
+<script>
+	import {
+		Button,
+		Container,
+		Col,
+		Row,
+		Card,
+		CardBody,
+		CardHeader,
+		CardTitle,
+		Input,
+		Form,
+		FormGroup,
+		Image
+	} from 'sveltestrap/src';
+
+	export let imgUrl;
+	export let text;
+    export let href;
+</script>
+
+<Button
+	href={href}
+	style="background-color:white; align:center; border-color:silver;"
+>
+	<div style="display: flex; justify-content:center; align-items: center; gap: 10px">
+		<Image src={imgUrl} style="height:28px;" />
+		<span style="color:gray;">{text}</span>
+	</div>
+</Button>
--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -1,20 +1,17 @@
 <script>
-  import {
-	  Button,
-	  Container,
-    Col,
-    Row,
+	import SocialLoginButton from '$lib/components/SocialLoginButton.svelte';
+	import {
+		Button,
+		Container,
+		Col,
+		Row,
 		Card,
 		CardBody,
-		CardFooter,
 		CardHeader,
-		CardSubtitle,
-		CardText,
 		CardTitle,
 		Input,
 		Form,
 		FormGroup,
-		Label,
 		Image
 	} from 'sveltestrap/src';
 
@@ -23,108 +20,128 @@
 		'/img/logo/veradi.png',
 		'/svg/login/veradi.svg',
 		'/img/login/email.png',
-		'/img/login/password.png'
+		'/img/login/password.png',
+		'/img/login/google.png',
+		'/img/login/kakao.png'
 	];
-
-	const placeholder = [
-		'<Image alt="veardiLogo" src="{img[2]}" style="height:40px; width:auto; margin-top:5px; margin-left:-10px"/>'
-	]
-
-	const login = async () => {
-		const res = await fetch('api/login', {
-			method: 'POST',
-			body: JSON.stringify({
-				username: 'test@test.com',
-				password: '12345678'
-			})
-		});
-	};
 </script>
 
 <Container class="py-4">
-  <Row>
-  	<Col xs="1" sm="1" md="2" lg="3" xl="3" />
-    <Col xs="10" sm="10" md="8" lg="6" xl="6">
-  	<Card style="align:center;">
-  	  <CardHeader>
-  	  	<CardTitle>로그인</CardTitle>
-  	  </CardHeader>
-  	  <CardBody>
-  	  	<Row>
-  	  		<Col xs="1" sm="1" md="1" lg="1" xl="1" />
-  	  		<Col xs="10" sm="10" md="10" lg="10" xl="10">
-						<Form>
-  	  				<Row class="pb-3" style="text-align:center">
-								<Image alt="veardiLogo" src="{img[1]}" style="height:45px; width:auto; margin-left:-15px;"/> 
-								<Image alt="veardiLogo" src="{img[2]}" style="height:40px; width:auto; margin-top:5px; margin-left:-10px"/> 
-							</Row>
-  	  				<Row class="color:lightgray;">Email Address</Row>
-  	  				<Row class="pb-3">
-  	  					<Input 
-									type="email" 
-            	    name="email"
-									id="exampleEmail"
-									placeholder="이메일"
-									style="
-										background-image: url({img[3]}) ;
-										background-size: 30px;
-										background-position: 10px;
-										background-repeat: no-repeat;
-										text-align: left;
-										text-indent: 40px;
-									"/>
-									
-  	  				</Row>
-  	  				<Row>Password</Row>
-  	  				<Row class="pb-2">
-  	  					<Input 
-								type="password" 
-								name="password"
-								id="examplePassword"
-								placeholder="비밀번호"
-								style="
-									background-image: url({img[4]}) ;
-									background-size: 25px;
-									background-position: 12px;
-									background-repeat: no-repeat;
-									text-align: left;
-									text-indent: 40px;
-								"/>
-  	  				</Row>
-  	  				<Row class="pb-3">
-  	  					<Col>
-									<FormGroup style="margin-left:-12px;">
-										<Input id="c1" type="checkbox" label="로그인 상태 유지"/>
-									</FormGroup>
-								</Col>
-  	  					<Col align="right" style="margin-right:-10px;">
-									<a href="http://w3schools.com" style="margin-right:8px; color:gray;">회원가입</a>
-									<a href="http://w3schools.com" style="color:gray;">ID/PW 찾기</a>
-								</Col>
-  	  				</Row>
-  	  				<Row class="py-2">
-  	  					<Col xs="1" sm="1" md="1" lg="1" xl="1" />
-  	  					<Col>
-  	  						<Row>
-  	  							<Button style="align:center; background-color:#42B9FF;">로그인</Button>
-  	  						</Row>
-  	  					</Col>
-  	  					<Col xs="1" sm="1" md="1" lg="1" xl="1" />
-  	  				</Row>
-  	  				<Row class="py-3">
-								<hr />
-  	  				</Row>
-  	  				<Row>
-  	  					<Col style="text-align:center;">Google</Col>
-  	  					<Col style="text-align:center;">KaKao</Col>
-  	  				</Row>
-						</Form>
-  	  		</Col>
-  	  		<Col xs="1" sm="1" md="1" lg="1" xl="1" />
-  	  	</Row>
-  		</CardBody>
-  	</Card>
-  	</Col>
-  </Row>
-  <Col xs="1" sm="1" md="2" lg="3" xl="3" />
+	<Row>
+		<Col xs="0" sm="1" md="2" lg="3" xl="3" />
+		<Col xs="12" sm="10" md="8" lg="6" xl="6">
+			<Card style="align:center; box-shadow: 2px 2px 7px;">
+				<CardHeader>
+					<CardTitle style="text-align:center; margin-top:5pt;">로그인</CardTitle>
+				</CardHeader>
+				<CardBody>
+					<Row>
+						<Col xs="1" sm="1" md="1" lg="1" xl="1" />
+						<Col xs="10" sm="10" md="10" lg="10" xl="10">
+							<Form>
+								<Row class="pb-3" style="text-align:center">
+									<Image
+										alt="veardiLogo"
+										src={img[1]}
+										style="height:45px; width:auto; margin-left:-15px;"
+									/>
+									<Image
+										alt="veardiLogo"
+										src={img[2]}
+										style="height:40px; width:auto; margin-top:5px; margin-left:-10px"
+									/>
+								</Row>
+								<Row class="color:lightgray;">Email Address</Row>
+								<Row class="pb-3">
+									<Input
+										type="email"
+										name="email"
+										id="exampleEmail"
+										placeholder="이메일"
+										style="
+		  								background-image: url({img[3]}) ;
+		  								background-size: 30px;
+		  								background-position: 10px;
+		  								background-repeat: no-repeat;
+		  								text-align: left;
+		  								text-indent: 40px;
+											
+		  							"
+									/>
+								</Row>
+								<Row>Password</Row>
+								<Row class="pb-2">
+									<Input
+										type="password"
+										name="password"
+										id="examplePassword"
+										placeholder="비밀번호"
+										style="
+		  							background-image: url({img[4]}) ;
+		  							background-size: 25px;
+		  							background-position: 12px;
+		  							background-repeat: no-repeat;
+		  							text-align: left;
+		  							text-indent: 40px;
+	 	  					"
+									/>
+								</Row>
+								<Row class="pb-3">
+									<Col
+										xs="12"
+										sm="6"
+										md="5"
+										style="padding-right:0px; margin-bottom:-15px; padding-bottom:-15px;"
+									>
+										<FormGroup style="margin-left:-12px; font-size:16px;">
+											<Input id="c1" type="checkbox" label="로그인 상태 유지" />
+										</FormGroup>
+									</Col>
+									<Col xs="12" sm="6" md="7" align="right" style="margin-right:-13px;">
+										<Row>
+											<Col xs="12" sm="12" md="6">
+												<a
+													href="http://w3schools.com"
+													style="color:gray; margin-right:-12px; padding-right:0px; font-size:15px; letter-spacing:-1px; text-align:right;"
+													>회원가입</a
+												>
+											</Col>
+											<Col xs="12" sm="12" md="6">
+												<a
+													href="http://w3schools.com"
+													style="color:gray; margin-right:-12px; padding-right:0px; font-size:15px; letter-spacing:-1px; padding-left:-5px"
+													>ID/PW 찾기</a
+												>
+											</Col>
+										</Row>
+									</Col>
+								</Row>
+								<Row class="py-1">
+									<Col xs="1" sm="1" md="1" lg="1" xl="1" />
+									<Col>
+										<Row>
+											<Button
+												style="align:center; background-color:#42B9FF; border:none; border-radius:21px;"
+												>로그인</Button
+											>
+										</Row>
+									</Col>
+									<Col xs="1" sm="1" md="1" lg="1" xl="1" />
+								</Row>
+								<Row class="py-1 pt-3">
+									<hr />
+								</Row>
+								<Row style="gap:7px;">
+										<SocialLoginButton href="http://www.google.com" imgUrl={img[5]} text="Google로 로그인" />
+										<SocialLoginButton href="http://www.kakao.com" imgUrl={img[6]} text="Kakao로 로그인" />
+								</Row>
+							</Form>
+						</Col>
+						<Col xs="1" sm="1" md="1" lg="1" xl="1" />
+					</Row>
+				</CardBody>
+			</Card>
+		</Col>
+		<Col xs="0" sm="1" md="2" lg="3" xl="3" />
+	</Row>
 </Container>
