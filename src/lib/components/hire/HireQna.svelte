1e665d9e5ec0b0806b902d29552eda72f7c29913
nospacethere <nospacethere@gmail.com>
2022-05-07 19:39:02 +0900

    현섭 : QNA페이지

--- src/lib/components/hire/HireQna.svelte
+++ src/lib/components/hire/HireQna.svelte
@@ -21,16 +21,16 @@
 	export let questionContents = 'contents';
 	export let answerContents = 'contents';
 
-  import img_iconQ from '$lib/static/img/hire/qna/iconQ.svg';
-  import img_iconA from '$lib/static/img/hire/qna/iconA.svg';
+  import img_iconQ from '$lib/static/img/hire/qna/iconQ.png';
+  import img_iconA from '$lib/static/img/hire/qna/iconA.png';
 </script>
 
 <AccordionItem>
 	<Row slot="header">
-		<Col sm="2" md="1" lg="1" class="d-none d-sm-block">
-			<Image src={img_iconQ} style="width:100px; height:50px;" />
+		<Col sm="2" md="2" lg="2" class="d-none d-sm-block">
+			<Image src={img_iconQ} style="width:50px; height:50px;" />
 		</Col>
-		<Col sm="10" md="11" lg="11">
+		<Col sm="10" md="10" lg="10">
 			<h5>
 				<b style="color:grey;">[{questionTitle}]</b>&nbsp
 				{questionContents}
@@ -38,10 +38,10 @@
 		</Col>
 	</Row>
 	<Row>
-		<Col sm="2" md="1" lg="1" class="d-none d-sm-block">
-			<Image src={img_iconA} style="width:100px; height:50px;" />
+		<Col sm="2" md="2" lg="2" class="d-none d-md-block">
+			<Image src={img_iconA} style="width:50px; height:50px; margin-left:40px;" />
 		</Col>
-		<Col sm="10" md="11" lg="11">
+		<Col sm="10" md="10" lg="10">
 			<h5>
 				{answerContents}
 			</h5>
