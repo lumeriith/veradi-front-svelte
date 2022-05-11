2e5e0b82efdbe3f6a1c0585276608996eeee0743
Eunseop Shim <lumeriith@gmail.com>
2022-05-12 02:31:10 +0900

    은섭:TailwindCSS의 breakpoint 크기를 Bootstrap과 동일하게 설정

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,19 @@
 module.exports = {
-  // add this section
-  prefix: 'tw-',
-  content: [
-    './src/**/*.html',
-    './src/**/*.svelte'
-  ],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-  important: true,
-}
+	// add this section
+	prefix: 'tw-',
+	content: ['./src/**/*.html', './src/**/*.svelte'],
+	theme: {
+		screens: {
+			sm: '576px',
+			md: '768px',
+			lg: '992px',
+			xl: '1200px'
+		},
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: [],
+	important: true
+};
