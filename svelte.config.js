26a653dfb2c7eab501c18d4549c622e5c4389ab7
Eunseop Shim <lumeriith@gmail.com>
2022-05-14 21:56:48 +0900

    은섭:SSR 관련 설정 수정

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,7 @@ import AutoImport from 'unplugin-auto-import/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		prerender: { default: true },
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
