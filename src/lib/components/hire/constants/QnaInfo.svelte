dd4c406a8c7f1374775d7410a8c9b5ea000ea9a6
Eunseop Shim <lumeriith@gmail.com>
2022-05-16 19:39:44 +0900

    은섭:자주묻는질문 페이지 내용 변경

--- src/lib/components/hire/constants/QnaInfo.svelte
+++ src/lib/components/hire/constants/QnaInfo.svelte
@@ -1,19 +1,40 @@
 <script context="module">
 	export const QnaInfo = [
 		{
-			questionTitle: '추가모집',
-			questionContents: '1차 모집 공고는 마무리됐습니다. 다음 공고는 나중을 기약해주세요. 1차 모집 공고는 마무리됐습니다. 다음 공고는 나중을 기약해주세요. 1차 모집 공고는 마무리됐습니다. 다음 공고는 나중을 기약해주세요.',
-			answerContents: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque expedita in sapiente, suscipit praesentium facere dolores maxime. Omnis, adipisci culpa dolore, veritatis ad quasi optio, reprehenderit veniam deserunt tenetur illo.'
+			questionTitle: '지원/면접',
+			questionContents: '지원서가 정상적으로 제출되었는지 어떻게 확인할 수 있을까요?',
+			answerContents:
+				'서류 지원 후 접수 완료 메일을 받으셨다면 정상적으로 제출된 상태입니다. 지원서 제출에 대한 문의 사항이 있으시면 example@example.com로 문의해 주세요.'
 		},
-    {
-			questionTitle: '추가모집',
-			questionContents: '2차 모집 공고는 마무리됐습니다. 다음 공고는 나중을 기약해주세요.',
-			answerContents: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque expedita in sapiente, suscipit praesentium facere dolores maxime. Omnis, adipisci culpa dolore, veritatis ad quasi optio, reprehenderit veniam deserunt tenetur illo.'
+		{
+			questionTitle: '지원/면접',
+			questionContents: '인터뷰는 어떻게 진행되나요?',
+			answerContents:
+				'인터뷰는 원칙적으로는 대면으로 진행합니다. 그러나 현 펜데믹 사태가 완화될 때 까지는 비대면으로 진행하고 있습니다. 자세한 내용은 채용 안내(채용안내 랜딩으로 연결할것)를 확인 해주세요.'
 		},
-    {
-			questionTitle: '추가모집',
-			questionContents: '3차 모집 공고는 마무리됐습니다. 다음 공고는 나중을 기약해주세요.',
-			answerContents: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque expedita in sapiente, suscipit praesentium facere dolores maxime. Omnis, adipisci culpa dolore, veritatis ad quasi optio, reprehenderit veniam deserunt tenetur illo.'
+		{
+			questionTitle: '지원/면접',
+			questionContents: '경력기술서 양식은 어디서 확인할 수 있나요?',
+			answerContents:
+				'자유 양식으로 경력기술서를 작성해 주세요. 지원 양식이 있는 포지션의 경우, 별도로 안내해 드리고 있습니다.'
 		},
+		{
+			questionTitle: '지원/면접',
+			questionContents: '전형 결과는 언제쯤 알 수 있나요? 불합격자에게도 통보가 되나요?',
+			answerContents:
+				'서류 전형 포함 모든 전형 단계에서 영업일 기준 최소 3일 최대 7일 내로 합격/ 불합격 여부를 안내해 드리고 있습니다. 소요 시간은 포지션에 따라 상이할 수 있으며, 부득이하게 결과 안내가 늦어지는 경우 별도로 연락을 드리고 있습니다.'
+		},
+		{
+			questionTitle: '기타',
+			questionContents: '인턴/ 계약직으로 입사한 경우 정규직 전환이 가능한가요?',
+			answerContents:
+				'모든 포지션에서 정규직을 전환을 고려하는 것은 아닙니다. 그러나 스스로의 러닝커브와 역량을 증명하면 그에 맞는 포지션을 찾아 제안해 드리고 있습니다. 당장 오픈 포지션이 없다면 기간 연장을 통해서 더 오래 함께 일하는 방법을 고민합니다. '
+		},
+		{
+			questionTitle: '기타',
+			questionContents: '베라디에 대해 좀 더 알아보고 싶다면 어디를 참고하면 좋을까요?',
+			answerContents:
+				'베라디 메인 페이지에는 베라디를 구성하는 학업 팀과  출판물, 제공하는 서비스 등 다양한 정보를 확인하실 수 있습니다. 그 중에서도 베라디의 학업 팀과 출판물 컨텐츠를 확인해 보시는 것을 추천 드립니다.'
+		}
 	];
 </script>
