573aeba3206654421d95e2ac6b8afc134f4b0a87
Eunseop Shim <lumeriith@gmail.com>
2022-05-21 03:43:46 +0900

    은섭:순서대로 협력 가능한 스크롤 애니메이션 위해 scrollEffects 제작

---
+++ src/lib/utils/scrollEffects.js
@@ -0,0 +1,64 @@
+import './scrollEffects.css';
+
+let io = null;
+
+function observationCallback(entries) {
+	entries.forEach((entry) => {
+		const target = entry.target;
+		if (entry.isIntersecting && !target.classList.contains('se-trigger')) {
+			target.classList.add('se-trigger');
+		} else if (!entry.isIntersecting && target.classList.contains('se-trigger')) {
+			target.classList.remove('se-trigger');
+		}
+	});
+}
+
+function initObserver() {
+	if (io) return;
+	io = new IntersectionObserver(observationCallback, {
+		rootMargin: '-50px'
+	});
+}
+
+function startObserving(target) {
+	initObserver();
+	io.observe(target);
+}
+
+function stopObserving(target) {
+	io.unobserve(target);
+}
+
+export function seTrigger(node) {
+	startObserving(node);
+	return {
+		destroy: () => stopObserving(node)
+	};
+}
+
+function valOrDefault(val, defVal) {
+	if (typeof val !== 'undefined') return val;
+	return defVal;
+}
+
+// ============= Animation Code =============
+// 새 애니메이션 추가하려면 이 아래에서 하시면 됩니다.
+
+export function seFlyUp(node, settings = { delay: 0, duration: 500, offset: 30 }) {
+	node.classList.add('se-animated');
+	node.classList.add('se-flyup');
+	node.style.setProperty('--se-delay', valOrDefault(settings.delay, 0) + 'ms');
+	node.style.setProperty('--se-duration', valOrDefault(settings.duration, 500) + 'ms');
+	node.style.setProperty('--se-offset', valOrDefault(settings.offset, 30) + 'px');
+}
+
+// ============== Preset Code ================
+// 애니메이션 자체가 아닌 Semantic한 의미의 프리셋이 정의된 공간입니다.
+
+export function seHeading(node, index = 0) {
+	return seFlyUp(node, { delay: index * 200 });
+}
+
+export function seBody(node, index = 0) {
+	return seFlyUp(node, { delay: index * 125 });
+}
