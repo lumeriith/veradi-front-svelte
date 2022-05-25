765282539e7f2361d999669c313acbf07094bf21
Eunseop Shim <lumeriith@gmail.com>
2022-05-26 04:04:06 +0900

    은섭:회사소개 하단 역량 파트 추가

--- src/routes/hire/index.svelte
+++ src/routes/hire/index.svelte
@@ -9,7 +9,8 @@
 	import HireCards from '$lib/components/hire/HireCards.svelte';
 
 	import img_mainPhoto from '$lib/static/img/introduction/mainPhoto.png';
-	import img_test from '$lib/static/img/test.png';
+	import img_bottomBg from '$lib/static/img/hire/index/bottomBg.jpg';
+
 	import HireGrowth from '$lib/components/hire/HireGrowth.svelte';
 	import NarrowContainer from '$lib/components/NarrowContainer.svelte';
 	import HireVision from '$lib/components/hire/HireVision.svelte';
@@ -84,4 +85,34 @@
 	<div class="tw-h-16" />
 	<HireCards />
 </NarrowContainer>
+<div class="tw-h-16 md:tw-h-20" />
+<!------------------------------------------------------------------------------>
+<div
+	class="tw-relative tw-bg-cover tw-bg-center tw-h-[250px] md:tw-h-[380px] lg:tw-h-[450px]"
+	style="background-image:url('{img_bottomBg}');"
+>
+	<div class="tw-absolute tw-bg-[#0000003f] tw-inset-0" />
+	<div class="tw-absolute tw-inset-0 tw-z-10 tw-flex tw-items-center tw-text-white tw-text-center">
+		<NarrowContainer>
+			<header
+				class="tw-font-heading tw-flex tw-flex-col tw-items-center"
+				in:fade={{ duration: 1000 }}
+			>
+				<div class="tw-text-3xl md:tw-text-5xl">당신의 역량을 펼치세요</div>
+				<div class="tw-text-xl md:tw-text-3xl">베라디에서, 뜨겁게 노력하고 힘차게 비상합시다</div>
+			</header>
+			<div class="tw-h-4 md:tw-h-8 lg:tw-h-12" />
+			<div
+				class="tw-text-md md:tw-text-lg tw-flex tw-flex-col tw-items-center tw-gap-2 md:tw-gap-3 tw-font-semibold"
+			>
+				<div in:fly={{ duration: 700, delay: 400, y: 30 }}>
+					우리는 혁신을 거듭하고, 리더를 양산하며, 지식을 나눕니다.
+				</div>
+				<div in:fly={{ duration: 700, delay: 700, y: 30 }}>
+					누구보다 치열한 20대를 기꺼이 이 곳, 베라디에서 함께하고 있습니다.
+				</div>
+			</div>
+		</NarrowContainer>
+	</div>
+</div>
 <div class="tw-h-16 md:tw-h-20" />
