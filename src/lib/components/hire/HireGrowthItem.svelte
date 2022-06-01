746dacf3c9e6a4fd5a4497d0d1be2a55e73328fd
Eunseop Shim <lumeriith@gmail.com>
2022-06-01 18:30:12 +0900

    은섭:회사소개 성장 항목에 선 긋는 애니메이션 추가

--- src/lib/components/hire/HireGrowthItem.svelte
+++ src/lib/components/hire/HireGrowthItem.svelte
@@ -1,5 +1,7 @@
 <script>
-	import { animHeading } from '$lib/utils/scrollEffects';
+	import { animFly } from '$lib/utils/scrollEffects';
+	import { onMount } from 'svelte';
+	import { scale } from 'svelte/transition';
 
 	export let info = { img: '', title: '', content: '' };
 	let _class = '';
@@ -7,9 +9,9 @@
 	export { _class as class };
 </script>
 
-<div class="md:tw-flex-1 tw-max-w-[230px] {_class} tw-flex tw-flex-col" {style}>
+<div class="md:tw-flex-1 tw-max-w-[230px] {_class} tw-flex tw-flex-col tw-overflow-hidden" {style}>
 	<img class="tw-w-14 tw-ml-auto" src={info.img} alt="" />
-	<div class="tw-bg-[#7fc4fd] tw-h-1 tw-my-2" />
+	<div class="tw-bg-[#7fc4fd] tw-h-1 tw-my-2" use:animFly={{ x: -200, duration: 1300 }} />
 	<header class="tw-font-bold tw-text-xl tw-mb-3">{info.title}</header>
 	<main class="tw-text-lg" style="line-height: 170%;">{info.content}</main>
 </div>
