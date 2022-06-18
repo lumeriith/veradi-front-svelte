5354859eb3ca281afebb934b2833eb1fed18325b
Eunseop Shim <lumeriith@gmail.com>
2022-06-18 13:47:35 +0900

    은섭:채용안내 페이지 버튼 링크 연결 안된 문제 해결

--- src/lib/components/hire/notice/HireSubjects.svelte
+++ src/lib/components/hire/notice/HireSubjects.svelte
@@ -7,22 +7,28 @@
 	class="tw-flex tw-gap-x-2 lg:tw-gap-x-12 tw-gap-y-2 lg:tw-gap-y-6 tw-flex-wrap tw-justify-center lg:tw-justify-start"
 >
 	{#each SubjectContents as subject, i}
-		<button
+		<a
 			href={subject.src}
 			class="tw-pt-4 tw-pb-3 tw-w-28 lg:tw-w-36 tw-h-auto tw-flex tw-flex-col tw-justify-center tw-items-center tw-text-center tw-rounded-xl"
 			use:animScale={{ delay: 100 * i }}
 		>
 			<img class="tw-w-16 lg:tw-w-20 tw-mb-4" src={subject.img} alt="" />
 			<div class="tw-font-semibold tw-text-lg lg:tw-text-xl">{subject.title}</div>
-		</button>
+		</a>
 	{/each}
 </div>
 
 <style>
-	button {
+	a {
+		color: #333;
+		text-decoration: none;
 	}
 
-	button:hover {
+	a:hover {
 		background-color: #eee;
+	}
+
+	a:active {
+		background-color: #ddd;
 	}
 </style>
