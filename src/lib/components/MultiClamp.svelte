e8eacf9dde0f314e0d62faa1718a62e178cc2092
Eunseop Shim <lumeriith@gmail.com>
2022-06-28 21:50:35 +0900

    은섭:multi-clamp 패키지 설치 및 Svelte Wrapper 제작

---
+++ src/lib/components/MultiClamp.svelte
@@ -0,0 +1,34 @@
+<script>
+	import MultiClamp from 'multi-clamp';
+	import { onMount } from 'svelte';
+
+	let container;
+	let _class;
+
+	export { _class as class };
+	export let style;
+
+	export let ellipsis = '...';
+	export let clamp = 3;
+	export let reverse = false;
+	export let splitByWords = false;
+	export let disableCssClamp = false;
+	export let onClampStart = () => {};
+	export let onClampEnd = () => {};
+
+	onMount(() => {
+		new MultiClamp(container, {
+			ellipsis,
+			clamp,
+			reverse,
+			splitByWords,
+			disableCssClamp,
+			onClampStart,
+			onClampEnd
+		});
+	});
+</script>
+
+<div bind:this={container} class={_class} {style}>
+	<slot />
+</div>
