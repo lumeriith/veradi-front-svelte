decfc2742a417bf2b8eb987a38a41111b6241f88
Eunseop Shim <lumeriith@gmail.com>
2022-07-13 01:43:51 +0900

    은섭:QnA 별점 기능 추가

---
+++ src/lib/components/question/item/AnswerRating.svelte
@@ -0,0 +1,94 @@
+<script>
+	import Icon from '@iconify/svelte';
+	import { fade, scale } from 'svelte/transition';
+
+	export let comment = {
+		id: 912,
+		type: 1,
+		content: getTestString('answer', 10, 60) + '\n\n' + getTestString('answer', 10, 80),
+		photos: [],
+		rating: -1
+	};
+
+	let isConfirmOpen = false;
+	let hoverRating = 3;
+
+	function hoverOutOfStars() {
+		if (isConfirmOpen) return;
+		hoverRating = 3;
+	}
+
+	function hoverInStar(i) {
+		if (isConfirmOpen) return;
+		hoverRating = i + 1;
+	}
+
+	function clickStar(i) {
+		hoverRating = i + 1;
+		isConfirmOpen = true;
+	}
+
+	function giveRating() {
+		comment.rating = hoverRating;
+		isConfirmOpen = false;
+	}
+</script>
+
+<div class="tw-mt-2 tw-flex tw-gap-2 tw-items-center">
+	{#if comment.rating > 0}
+		<span class="tw-opacity-50 tw-text-sm">평가해주셔서 감사합니다.</span>
+		<span class="tw-flex tw-text-lg tw-text-[#ffc400] tw-opacity-70">
+			{#each [0, 1, 2, 3, 4] as i (i)}
+				<div>
+					{#if comment.rating > i}
+						<Icon icon="ant-design:star-filled" />
+					{:else}
+						<Icon icon="ant-design:star-outlined" />
+					{/if}
+				</div>
+			{/each}
+		</span>
+	{:else}
+		<span class="tw-opacity-60 tw-text-sm">답변에 만족하시나요?</span>
+		<span class="tw-flex tw-text-lg tw-text-[#ffc400]" on:mouseleave={hoverOutOfStars}>
+			{#each [0, 1, 2, 3, 4] as i (i)}
+				<div
+					class="tw-cursor-pointer"
+					on:mouseenter={() => hoverInStar(i)}
+					on:click={() => clickStar(i)}
+				>
+					{#if hoverRating > i}
+						<Icon icon="ant-design:star-filled" />
+					{:else}
+						<Icon icon="ant-design:star-outlined" />
+					{/if}
+				</div>
+			{/each}
+		</span>
+	{/if}
+</div>
+
+{#if isConfirmOpen}
+	<div class="tw-text-sm tw-flex tw-gap-3">
+		<div class="tw-opacity-60">정말 이 답변에 {hoverRating}점을 주시겠습니까?</div>
+		<div class="tw-flex tw-gap-3">
+			<button on:click={giveRating}>별점주기</button>
+			<button on:click={() => (isConfirmOpen = false)}>취소</button>
+		</div>
+	</div>
+{/if}
+
+<style>
+	button {
+		color: var(--book-qna-primary);
+		text-decoration: underline;
+	}
+
+	button:hover {
+		filter: brightness(1.2);
+	}
+
+	button:active {
+		filter: brightness(0.9);
+	}
+</style>
--- src/lib/components/question/item/QuestionItemContent.svelte
+++ src/lib/components/question/item/QuestionItemContent.svelte
@@ -4,6 +4,7 @@
 	import { Jumper } from 'svelte-loading-spinners';
 	import { fade } from 'svelte/transition';
 	import BookQnaAnswerMark from './AnswerMark.svelte';
+	import AnswerRating from './AnswerRating.svelte';
 	import BookQnaQuestionMark from './QuestionMark.svelte';
 
 	export let id = '';
@@ -19,21 +20,35 @@
 		photos: [],
 		comments: [
 			{
+				id: 912,
 				type: 1,
 				content: getTestString('answer', 10, 60) + '\n\n' + getTestString('answer', 10, 80),
-				photos: []
+				photos: [],
+				rating: 4
 			},
 			{
+				id: 911,
 				type: 0,
-				content: getTestString('question', 8, 15)
+				content: getTestString('question', 8, 15),
+				rating: -1
 			},
 			{
+				id: 918,
 				type: 1,
 				content: getTestString('answer', 10, 40),
-				photos: []
+				photos: [],
+				rating: -1
 			}
 		]
 	});
+
+	function isCommentQuestion(c) {
+		return c.type === 0;
+	}
+
+	function isCommentAnswer(c) {
+		return c.type === 1;
+	}
 </script>
 
 {#await request}
@@ -48,13 +63,18 @@
 		{#each response.comments as comment}
 			<div class="tw-flex">
 				<div class="tw-w-7">
-					{#if comment.type === 0}
+					{#if isCommentQuestion(comment)}
 						<BookQnaQuestionMark />
 					{:else}
 						<BookQnaAnswerMark />
 					{/if}
 				</div>
-				<div class="tw-pt-1 tw-ml-1 tw-flex-1" style="line-height: 160%;">{comment.content}</div>
+				<div class="tw-pt-1 tw-ml-1 tw-flex-1" style="line-height: 160%;">
+					{comment.content}
+					{#if isCommentAnswer(comment)}
+						<AnswerRating {comment} />
+					{/if}
+				</div>
 			</div>
 		{/each}
 	</div>
