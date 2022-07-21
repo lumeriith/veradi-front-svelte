483ccf841ee756129f285da324a8f5fb207a1e40
Eunseop Shim <lumeriith@gmail.com>
2022-07-22 03:07:49 +0900

    은섭:서버 코드 추가

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,9 +1,45 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const firestore = admin.firestore();
+
+exports.createUserRole = functions.auth.user().onCreate(async (user) => {
+	await firestore.collection('users').doc(user.uid).create({ role: 'student' });
+	return { success: true };
+});
+
+exports.removeUserRole = functions.auth.user().onDelete(async (user) => {
+	await firestore.collection('users').doc(user.uid).delete();
+	return { success: true };
+});
+
+exports.ping = functions.https.onCall(() => {
+	return { pong: 'pong!' };
+});
+
+exports.deleteBookQuestion = functions
+	.runWith({
+		timeoutSeconds: 30,
+		memory: '128MB'
+	})
+	.https.onCall(async (data, context) => {
+		const { questionId } = data;
+		if (!context.auth || !context.auth.uid) {
+			throw new functions.https.HttpsError('인증 오류', '로그인이 필요합니다.');
+		}
+		const docRef = firestore.collection('bookQuestions').doc(questionId);
+		const doc = await docRef.get();
+		if (doc.get('author') != context.auth.uid) {
+			throw new functions.https.HttpsError('권한 오류', '자신의 게시물만 삭제할 수 있습니다.');
+		}
+
+		console.log(`${context.auth.uid} 유저가 bookQuestions/${questionId}를 삭제합니다.`);
+
+		await firestore.recursiveDelete(docRef);
+
+		return {
+			success: true
+		};
+	});
