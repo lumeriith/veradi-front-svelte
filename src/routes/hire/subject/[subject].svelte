aca5ed8195576e35bed10457f2586d667fb61ed1
Eunseop Shim <lumeriith@gmail.com>
2022-07-21 18:13:26 +0900

    은섭:채용공고 페이지 리팩토링 통한 중복 코드 제거

---
+++ src/routes/hire/subject/[subject].svelte
@@ -0,0 +1,20 @@
+<script>
+	import HireSubjectAnnouncement from '$lib/components/hire/HireSubjectAnnouncement.svelte';
+	import { page } from '$app/stores';
+	import subjectHire from '$lib/data/hire/subjectHire';
+	import veradiUrls from '$lib/data/veradiUrls';
+
+	$: subjectId = $page.params.subject;
+	$: subject = subjectHire[subjectId];
+</script>
+
+<HireSubjectAnnouncement
+	subjectName={subject.subjectName}
+	subjectIntro={subject.subjectIntro}
+	subjectIntroUrl={veradiUrls.introduction[subjectId]}
+	applicationDate={subject.applicationDate}
+	applicationIntake={subject.applicationIntake}
+	task={subject.task.join('\n')}
+	requirement={subject.requirement.join('\n')}
+	prefer={subject.prefer.join('\n')}
+/>
