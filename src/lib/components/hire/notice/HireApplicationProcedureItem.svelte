0afb3be34f28dd772b045f169321d3762b8456d9
Eunseop Shim <lumeriith@gmail.com>
2022-07-30 17:15:47 +0900

    은섭:채용안내 페이지 모바일 대응 수정

--- src/lib/components/hire/notice/HireApplicationProcedureItem.svelte
+++ src/lib/components/hire/notice/HireApplicationProcedureItem.svelte
@@ -5,9 +5,9 @@
 </script>
 
 <div
-	class="tw-w-full lg:tw-w-32 tw-aspect-square tw-rounded-full tw-flex tw-flex-col tw-justify-center tw-items-center"
+	class="tw-w-full lg:tw-w-32 tw-aspect-square tw-rounded-full tw-flex tw-flex-col tw-justify-center tw-items-center tw-text-center"
 	style="background-color: {bg};"
 >
 	<div class="tw-text-white tw-text-lg lg:tw-text-xl tw-hidden sm:tw-block">0{index + 1}</div>
-	<div class="tw-text-white tw-text-md sm:tw-text-lg lg:tw-text-xl">{text}</div>
+	<div class="tw-text-white tw-text-xs sm:tw-text-lg lg:tw-text-xl">{text}</div>
 </div>
